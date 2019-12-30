a5c2a683955f3c74a68f8e445b32442266671f14
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-12-30 17:49:40 +0000

    Dashboard Part 1 passing tests

--- src/language/language-router.js
+++ src/language/language-router.js
@@ -46,7 +46,26 @@ languageRouter
 languageRouter
   .get('/head', async (req, res, next) => {
     // implement me
-    res.send('implement me!');
+    try {
+      const language = await LanguageService.getUsersLanguage(
+        req.app.get('db'),
+        req.language.user_id
+      );
+      const words = await LanguageService.getLanguageWords(
+        req.app.get('db'),
+        req.language.id,
+      );
+      const currentWord = words.find(element => element.id === language.head);
+      const responseObject = {
+        nextWord: currentWord.original,
+        wordCorrectCount: currentWord.correct_count,
+        wordIncorrectCount: currentWord.incorrect_count,
+        totalScore: language.total_score
+      };
+      res.json(responseObject);
+    } catch(error) {
+      next(error);
+    }
   });
 
 languageRouter
